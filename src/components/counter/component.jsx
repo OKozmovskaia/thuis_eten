d7289b9f095963ae4877e87cf48a5cd769489deb
OKozmovskaia <olga.kozmovskaia@gmail.com>
2023-11-26 15:23:52 +0100

    Remove extra spaces (prettier)

--- src/components/counter/component.jsx
+++ src/components/counter/component.jsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
-
 export const Counter = ({ value, increment, decrement }) => {
   return (
     <div>
       <button onClick={decrement} disabled={value <= 0}>
-        {" "}
-        -{" "}
+        -
       </button>
       <span> {value} </span>
       <button onClick={increment} disabled={value >= 5}>
-        {" "}
-        +{" "}
+        +
       </button>
     </div>
   );
