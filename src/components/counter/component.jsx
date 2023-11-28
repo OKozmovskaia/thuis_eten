51601e8852a33b3ced65a867506df5a3d84d77af
OKozmovskaia <olga.kozmovskaia@gmail.com>
2023-11-28 21:18:56 +0100

    Add style to Counter

--- src/components/counter/component.jsx
+++ src/components/counter/component.jsx
@@ -1,11 +1,21 @@
+import styles from "./styles.module.css";
+
 export const Counter = ({ value, increment, decrement, min, max }) => {
   return (
     <div>
-      <button onClick={decrement} disabled={value <= min}>
+      <button
+        className={styles.button}
+        onClick={decrement}
+        disabled={value <= min}
+      >
         -
       </button>
-      <span> {value} </span>
-      <button onClick={increment} disabled={value >= max}>
+      <span className={styles.value}> {value} </span>
+      <button
+        className={styles.button}
+        onClick={increment}
+        disabled={value >= max}
+      >
         +
       </button>
     </div>
