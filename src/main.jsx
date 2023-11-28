910eb1aa362726f198cc06f473b0d1e4f78ccca2
OKozmovskaia <olga.kozmovskaia@gmail.com>
2023-11-28 20:54:23 +0100

    Add index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { restaurants } from "./constants/entities";
 import { Page } from "./pages/main-page/component";
+import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
