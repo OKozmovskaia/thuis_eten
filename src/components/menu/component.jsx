dd3b7b78639e3c771ac3eea62c59d664123ad590
OKozmovskaia <olga.kozmovskaia@gmail.com>
2023-11-29 19:38:29 +0100

    Add style to Menu

--- src/components/menu/component.jsx
+++ src/components/menu/component.jsx
@@ -1,16 +1,17 @@
 import { Dish } from "../dish/component";
+import styles from "./styles.module.css";
 
 export const Menu = ({ menu }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <h3>Menu:</h3>
-      <ol>
+      <ul className={styles.list}>
         {menu.map((dish) => (
           <li key={dish.id}>
             <Dish dish={dish} />
           </li>
         ))}
-      </ol>
+      </ul>
     </div>
   );
 };
