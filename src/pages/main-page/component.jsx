8d2b222427a42ca9646b90d4268067f6dadd5c25
OKozmovskaia <olga.kozmovskaia@gmail.com>
2023-11-29 19:39:42 +0100

    Add style to RestaurantPage

--- src/pages/main-page/component.jsx
+++ src/pages/main-page/component.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { RestaurantsTabs } from "../../components/restaurants-tabs/component";
 import { Restaurant } from "../../components/restaurant/component";
+import styles from "./styles.module.css";
+import Logo from "../../images/logo.svg";
 
 export const Page = ({ restaurants }) => {
   const items = Array.from(
@@ -18,8 +20,11 @@ export const Page = ({ restaurants }) => {
   }
 
   return (
-    <div>
-      <h1>Our restaurants:</h1>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        thuis eten
+        <img src={Logo} alt="thuis-eten logo" width="40px" height="40px" />
+      </div>
       <RestaurantsTabs items={items} onSelectTab={setActiveRestaurantID} />
       {selectedRestaurant && <Restaurant restaurant={selectedRestaurant} />}
     </div>
