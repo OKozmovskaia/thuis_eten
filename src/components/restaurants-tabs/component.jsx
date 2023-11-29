e87d9088e92d50b0e3f3622033a283342beda0b5
OKozmovskaia <olga.kozmovskaia@gmail.com>
2023-11-29 19:39:22 +0100

    Add style to ReataurantsTabs

--- src/components/restaurants-tabs/component.jsx
+++ src/components/restaurants-tabs/component.jsx
@@ -1,8 +1,9 @@
 import { Tab } from "../tab/component";
+import styles from "./styles.module.css";
 
 export const RestaurantsTabs = ({ items, onSelectTab }) => {
   return (
-    <div>
+    <div className={styles.container}>
       {items.map(({ name, id }) => (
         <Tab key={id} onClick={() => onSelectTab(id)}>
           {name}
