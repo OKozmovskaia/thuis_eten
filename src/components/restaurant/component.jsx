4ea56a3fd4c777b61f593cdff366f7ffb3f7d0c5
OKozmovskaia <olga.kozmovskaia@gmail.com>
2023-11-29 19:39:01 +0100

    Add style to Restaurant

--- src/components/restaurant/component.jsx
+++ src/components/restaurant/component.jsx
@@ -1,15 +1,15 @@
 import { Menu } from "../menu/component";
 import { Reviews } from "../reviews/component";
+import styles from "./styles.module.css";
 
 export const Restaurant = ({ restaurant }) => {
   const { name, menu, reviews } = restaurant;
 
   return (
-    <div>
+    <div className={styles.container}>
       <h2>{name}</h2>
       <Menu menu={menu} />
       <Reviews reviews={reviews} />
-      <hr />
     </div>
   );
 };
