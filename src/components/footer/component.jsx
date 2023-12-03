5e04014d49ac5442b386ce740f375b88f9489d52
OKozmovskaia <olga.kozmovskaia@gmail.com>
2023-12-03 16:19:04 +0100

    Add Footer

---
+++ src/components/footer/component.jsx
@@ -0,0 +1,8 @@
+import classNames from "classnames";
+import styles from "./styles.module.css";
+
+export const Footer = ({ className }) => {
+  return (
+    <footer className={classNames(className, styles.container)}>Footer</footer>
+  );
+};
