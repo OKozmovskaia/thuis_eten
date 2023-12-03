06ac6e1a0f24ed00736924bcc8b0d815411ed2ad
OKozmovskaia <olga.kozmovskaia@gmail.com>
2023-12-03 20:06:32 +0100

    Add buttons to Header and Footer

--- src/components/footer/component.jsx
+++ src/components/footer/component.jsx
@@ -1,8 +1,11 @@
 import classNames from "classnames";
 import styles from "./styles.module.css";
+import { Button } from "../button/component";
 
 export const Footer = ({ className }) => {
   return (
-    <footer className={classNames(className, styles.container)}>Footer</footer>
+    <footer className={classNames(className, styles.container)}>
+      <Button>Contact</Button>
+    </footer>
   );
 };
--- src/components/header/component.jsx
+++ src/components/header/component.jsx
@@ -1,12 +1,20 @@
 import classNames from "classnames";
 import styles from "./styles.module.css";
 import Logo from "../../images/logo.svg";
+import { Button } from "../button/component";
 
 export const Header = ({ className }) => {
   return (
     <header className={classNames(className, styles.container)}>
-      thuis eten
-      <img src={Logo} alt="thuis-eten logo" width="40px" height="40px" />
+      <div className={styles.logo}>
+        thuis eten
+        <img src={Logo} alt="thuis-eten logo" width="40px" height="40px" />
+      </div>
+
+      <div>
+        <Button className={styles.button}>Change Theme</Button>
+        <Button>Order</Button>
+      </div>
     </header>
   );
 };
