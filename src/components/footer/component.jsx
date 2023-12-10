8cfe2872f7db483a0145986faac10463603d1ab8
OKozmovskaia <olga.kozmovskaia@gmail.com>
2023-12-10 10:52:16 +0100

    Add ThemeProviders overlap

--- src/components/footer/component.jsx
+++ src/components/footer/component.jsx
@@ -1,11 +1,14 @@
 import classNames from "classnames";
 import styles from "./styles.module.css";
 import { Button } from "../button/component";
+import { ThemeProvider } from "../theme/component";
 
 export const Footer = ({ className }) => {
   return (
     <footer className={classNames(className, styles.container)}>
-      <Button>Contact</Button>
+      <ThemeProvider>
+        <Button>Contact</Button>
+      </ThemeProvider>
     </footer>
   );
 };
